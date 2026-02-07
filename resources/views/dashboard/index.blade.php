301391a71bf2e614831e2cb7ee71b4550e7ca052
gibranfajar <gibransatritamaa@gmail.com>
2026-02-07 22:52:59 +0700

    update status badge deshboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -20,18 +20,6 @@
             <!-- [ Main Content ] start -->
             <div class="row">
                 <!-- [ sample-page ] start -->
-                {{-- <div class="col-md-6 col-xl-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h6 class="mb-2 f-w-400 text-muted">Total Page Views</h6>
-                            <h4 class="mb-3">4,42,236 <span class="badge bg-light-primary border border-primary"><i
-                                        class="ti ti-trending-up"></i> 59.3%</span></h4>
-                            <p class="mb-0 text-muted text-sm">You made an extra <span class="text-primary">35,000</span>
-                                this year
-                            </p>
-                        </div>
-                    </div>
-                </div> --}}
                 <div class="col-md-6 col-xl-4">
                     <div class="card">
                         <div class="card-body">
@@ -175,7 +163,7 @@
                                                         class="badge 
                                                             @if ($item->payment->status == 'pending') bg-warning 
                                                             @elseif($item->payment->status == 'settlement') bg-success 
-                                                            @elseif($item->payment->status == 'failed') bg-danger 
+                                                            @elseif($item->payment->status == 'expire') bg-danger 
                                                             @else bg-secondary @endif">
                                                         {{ ucfirst($item->payment->status) }}
                                                     </span>
